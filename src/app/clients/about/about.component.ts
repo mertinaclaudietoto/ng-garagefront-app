610098a4c0719d92f104aae004658a276a259a17
tafitasoa1949 <tafitasoaratsimbaha@gmail.com>
2025-03-17 11:37:55 +0300

    add about page

--- src/app/clients/about/about.component.ts
+++ src/app/clients/about/about.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { Timeline } from 'primeng/timeline';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { NgIf } from '@angular/common';
+
+interface EventItem {
+  status?: string;
+  date?: string;
+  icon?: string;
+  color?: string;
+  image?: string;
+}
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [Timeline, CardModule, ButtonModule, NgIf],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
+  events: EventItem[];
 
+  constructor() {
+    this.events = [
+      { status: 'Ordered', date: '15/10/2020 10:30', icon: 'pi pi-shopping-cart', color: '#9C27B0', image: 'game-controller.jpg' },
+      { status: 'Processing', date: '15/10/2020 14:00', icon: 'pi pi-cog', color: '#673AB7', image: 'game-controller.jpg' },
+      { status: 'Shipped', date: '15/10/2020 16:15', icon: 'pi pi-shopping-cart', color: '#FF9800', image: 'game-controller.jpg' },
+      { status: 'Delivered', date: '16/10/2020 10:00', icon: 'pi pi-check', color: '#607D8B', image: 'game-controller.jpg' }
+    ];
+  }
 }
