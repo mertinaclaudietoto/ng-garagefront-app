b4f72b2c37fbb7287b2067aacae57383f45d5b88
Tafitasoa RATSIMBAHARISON <118884621+tafitasoa1949@users.noreply.github.com>
2025-03-20 16:38:40 +0300

    add message component for notification

--- src/app/features/carType/components/car-type/car-type.component.ts
+++ src/app/features/carType/components/car-type/car-type.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { TableModule } from 'primeng/table';
 import { CarType } from '../../../../shared/models/carType.model';
 import { ButtonModule } from 'primeng/button';
@@ -9,10 +9,11 @@ import { Dialog } from 'primeng/dialog';
 import { InputTextModule } from 'primeng/inputtext';
 import { FormsModule } from '@angular/forms';
 import { InputNumber } from 'primeng/inputnumber';
+import { MessageComponent } from '../../../../shared/components/message/message.component';
 
 @Component({
   selector: 'app-car-type',
-  imports: [CommonModule, TableModule, ButtonModule, Dialog, InputTextModule, FormsModule, InputNumber],
+  imports: [CommonModule, TableModule, ButtonModule, Dialog, InputTextModule, FormsModule, InputNumber, MessageComponent],
   templateUrl: './car-type.component.html',
   styleUrl: './car-type.component.scss'
 })
@@ -20,6 +21,7 @@ export class CarTypeComponent implements OnInit, OnDestroy {
   carTypeList: CarType[] = [];
   private destroys$ = new Subject<void>();
   visibleDialog: boolean = false;
+  @ViewChild(MessageComponent) messageComponent!: MessageComponent;
 
   constructor(private cartTypeService: CarTypeService) { }
   ngOnInit(): void {
@@ -34,9 +36,11 @@ export class CarTypeComponent implements OnInit, OnDestroy {
     this.cartTypeService.deleteCarType(id).pipe(takeUntil(this.destroys$)).subscribe({
       next: (types) => {
         this.getAllCarTypes();
+        this.messageComponent.showMessage('success', 'Suppression réussie', 'Le type de voiture a été supprimé.');
       },
       error: (err) => {
         console.error('Error delete car types:', err)
+        this.messageComponent.showMessage('error', 'Erreur', 'Échec de la suppression du type de voiture.');
       }
     })
   }
