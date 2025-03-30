677b38ffe38c1449519eafed6190a7e7aaa9845a
Tafitasoa RATSIMBAHARISON <118884621+tafitasoa1949@users.noreply.github.com>
2025-03-30 21:02:21 +0300

    fix agenda calendar

--- src/app/features/mechanic/agenda/component/agenda.component.ts
+++ src/app/features/mechanic/agenda/component/agenda.component.ts
@@ -1,5 +1,4 @@
-import { NgFor } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CalendarModule } from 'primeng/calendar';
 import { FluidModule } from 'primeng/fluid';
@@ -11,7 +10,17 @@ import { CalendarOptions } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import frLocale from '@fullcalendar/core/locales/fr';
+import { Task } from '../../../../shared/models/task.model';
+import { AgendaService } from '../services/agenda.service';
+import { Subject, takeUntil } from 'rxjs';
+import { MessageComponent } from '../../../../shared/components/message/message.component';
+import { ConfirmationService } from 'primeng/api';
 
+interface EventCallendar {
+  serviceId: string;
+  title: string;
+  date: string
+}
 
 @Component({
   selector: 'app-agenda',
@@ -19,55 +28,92 @@ import frLocale from '@fullcalendar/core/locales/fr';
     CalendarModule,
     FluidModule,
     ScrollPanelModule,
-    NgFor,
     ButtonModule,
     ModalDetailTaskComponent,
     FullCalendarModule
   ],
+  providers: [ConfirmationService],
   templateUrl: './agenda.component.html',
   styleUrl: './agenda.component.scss'
 })
-export class AgendaComponent {
-  // date: Date[] | undefined;
-  // availableProducts: Task[] | undefined;
-  // modalVisible = false;
-  // selectedTask!: Task;
+export class AgendaComponent implements OnInit, OnDestroy {
+  modalVisible = false;
+  selectedTask!: Task;
+  events: any[] = [];
+  calendarOptions!: CalendarOptions;
+  private destroys$ = new Subject<void>();
+  @ViewChild(MessageComponent) messageComponent!: MessageComponent;
 
-  // ngOnInit() {
-  // }
+  constructor(private agendaService: AgendaService) { }
 
-  // selectDate(event: Date) {
-  //   console.log(event);
-  //   // this.availableProducts = tasks;
-  // }
-
-  // showModal(task: Task) {
-  //   this.selectedTask = task;
-  //   this.modalVisible = true;
-  // }
-  calendarOptions: CalendarOptions = {
-    initialView: 'dayGridMonth',
-    plugins: [dayGridPlugin, interactionPlugin],
-    locale: frLocale,
-    views: {
-      dayGridMonth: {}, // Ajoute cette ligne
-    },
-    dateClick: this.handleDateClick.bind(this), // Clic sur une date
-    eventClick: this.handleEventClick.bind(this),
-    events: [
-      { title: 'Véhicule : 234433', date: '2025-03-01' }, // Mets une date actuelle
-      { title: 'Véhicule: 2323', date: '2025-03-02' }
-    ]
-  };
-
-  constructor() {
+  ngOnInit(): void {
+    this.getAllTaskMechanic("000000000000000000000001");
+    this.calendarOptions = {
+      initialView: 'dayGridMonth',
+      plugins: [dayGridPlugin, interactionPlugin],
+      locale: frLocale,
+      views: {
+        dayGridMonth: {},
+      },
+      eventClick: this.handleEventClick.bind(this),
+      events: this.events
+    };
   }
 
-  handleDateClick(arg: any) {
-    alert('Date cliquée : ' + arg.dateStr);
+  ngOnDestroy(): void {
+    this.destroys$.next();
+    this.destroys$.complete();
   }
 
   handleEventClick(info: any) {
-    alert('Événement cliqué : ' + info.event.title + '\nDate : ' + info.event.startStr);
+    const serviceId = info.event._def.extendedProps.serviceId;
+    this.getTaskById(serviceId);
+  }
+
+  getAllTaskMechanic(id: string) {
+    this.agendaService.getAllTaskMechanic(id).pipe(takeUntil(this.destroys$)).subscribe({
+      next: (tasks) => {
+        const newEvents = tasks
+          .filter(task => task.datedebut)
+          .map(task => {
+            const formattedDate = this.castDateString(task.datedebut);
+            return {
+              serviceId: task._id,
+              title: `${task.carCustomer.model} : ${task.carCustomer.empathement}`,
+              date: formattedDate
+            } as EventCallendar;
+          });
+        this.events = [...this.events, ...newEvents];
+        this.calendarOptions = {
+          ...this.calendarOptions,
+          events: this.events
+        };
+      },
+      error: (err) => {
+        console.error(err)
+        this.messageComponent.showMessage('error', 'Erreur', 'Échec de la récupération liste tâches .');
+      }
+    })
+  }
+
+  getTaskById(taskId: string) {
+    this.agendaService.getTaskByiD(taskId).pipe(takeUntil(this.destroys$)).subscribe({
+      next: (task) => {
+        this.selectedTask = task;
+        this.modalVisible = true;
+      },
+      error: (err) => {
+        console.error(err)
+        this.messageComponent.showMessage('error', 'Erreur', 'Échec de la récupération de tâche id: ' + taskId);
+      }
+    })
+  }
+
+  castDateString(dateInput: string | Date): string {
+    const date = dateInput instanceof Date ? dateInput : new Date(dateInput);
+    const day = String(date.getUTCDate()).padStart(2, '0');
+    const month = String(date.getUTCMonth() + 1).padStart(2, '0');
+    const year = date.getUTCFullYear();
+    return `${year}-${month}-${day}`;
   }
 }
--- src/app/features/mechanic/agenda/services/agenda.service.ts
+++ src/app/features/mechanic/agenda/services/agenda.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { environement } from '../../../../../environement/environement';
+import { HttpClient } from '@angular/common/http';
+import { Task } from '../../../../shared/models/task.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AgendaService {
 
-  constructor() { }
+  private urlServiceClient = environement.apiUrl + 'services-client';
+  constructor(private http: HttpClient) { }
+
+  getAllTaskMechanic(mechanicId: string): Observable<Task[]> {
+    return this.http.get<Task[]>(this.urlServiceClient + '/all-task-mechanic/' + mechanicId);
+  }
+
+  getTaskByiD(taskId: string): Observable<Task> {
+    return this.http.get<Task>(this.urlServiceClient + '/task-detail/' + taskId);
+  }
 }
