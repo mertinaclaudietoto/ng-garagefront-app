1c7520a9bfb942c90686938ece4ae035371f241d
AlinaMertina <mertinaclaudietoto@gmail.com>
2025-04-01 14:29:47 +0300

    feature[app]:facture

--- src/app/features/costumers/historique/historique.component.ts
+++ src/app/features/costumers/historique/historique.component.ts
@@ -1,15 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { PanierviewService } from './../panierview/panierview.service';
-import { serviceprice, ServicesCarService } from './../../manager/services-car/serice/services-car.service';
-import { Component } from '@angular/core';
-import { CarService } from './../../costumers/services/car.service';
+import { Component, NgModule } from '@angular/core';
 import { FluidModule } from 'primeng/fluid';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { SelectModule } from 'primeng/select';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule,NgModel,Validators } from '@angular/forms';
 import { TextareaModule } from 'primeng/textarea';
 import { IconFieldModule } from 'primeng/iconfield';
-import { MenuItem } from 'primeng/api';
 // import 
 import { CarouselModule } from 'primeng/carousel';
 import { GalleriaModule } from 'primeng/galleria';
@@ -18,41 +16,41 @@ import { TagModule } from 'primeng/tag';
 import { InputIconModule } from 'primeng/inputicon';
 import { ToolbarModule } from 'primeng/toolbar';
 import { SplitButtonModule } from 'primeng/splitbutton';
-import { CarTypeComponent } from '../../../layout/car/car-type/car-type.component';
 import { TableModule } from 'primeng/table';
 import { DialogModule } from 'primeng/dialog';
-import { Car } from '../../../shared/models/car';
-import { IMAGESDEFAULTS } from '../../costumers/dataimage';
-
-
-// cloudinary
-
-import {fill} from "@cloudinary/url-gen/actions/resize";
-import {Cloudinary, CloudinaryImage} from '@cloudinary/url-gen';
-import { serviceCar } from './../../manager/services-car/serice/services-car.service'; 
-import { IdName } from '../../../shared/models/car-type';
-import { skip, Subscription } from 'rxjs';
 import { PaginatorModule, PaginatorState } from 'primeng/paginator';
-import { detserviceClient, serviceList } from '../serviceview/serviceview.component';
-import { ServiceviewService } from '../serviceview.service';
-import { response } from 'express';
+import { detserviceClient,  } from '../serviceview/serviceview.component';
+import { FormatnumberpipePipe } from '../../../shared/pipe/formatnumber/formatnumberpipe.pipe';
+export interface PaymentInfo {
+  name: string;        
+  numbercard: number;  
+  expiry: string;      
+  cvv: number;        
+}
 
 @Component({
   selector: 'app-historique',
-  imports: [InputTextModule, FluidModule, ButtonModule, SelectModule, FormsModule, TextareaModule, CarouselModule, ButtonModule, GalleriaModule, ImageModule, TagModule, IconFieldModule, InputIconModule, ToolbarModule, SplitButtonModule, CarTypeComponent,TableModule,DialogModule,PaginatorModule],
+  imports: [FormatnumberpipePipe,InputTextModule, FluidModule, ButtonModule, SelectModule, FormsModule, TextareaModule, CarouselModule, ButtonModule, GalleriaModule, ImageModule, TagModule, IconFieldModule, InputIconModule, ToolbarModule, SplitButtonModule, TableModule,DialogModule,PaginatorModule,CommonModule],
   templateUrl: './historique.component.html',
   styleUrl: './historique.component.scss'
 })
 export class HistoriqueComponent {
-  subscriptions : Subscription[] = [];
   servicelist:detserviceClient[]|undefined=undefined;
   modifivalue!:detserviceClient|null;
-  index:number=-1;
+  index:number=0;
   display: boolean = false;
+  displaypaiement:boolean=false;
   rowIndex:string='';
   first: number = 0;
   rows: number = 10;
-  constructor(private panier :PanierviewService) {
+  infopayement:PaymentInfo={
+    name:'',
+    numbercard:0,
+    expiry:'',
+    cvv:0,
+  }
+  
+  constructor(private  readonly panier :PanierviewService) {
   }
   ngOnInit() {
     const userId = localStorage.getItem('iduser');
@@ -66,14 +64,22 @@ export class HistoriqueComponent {
     this.modifivalue=null;
     this.index=0;
   }
+  closeInvoice() {
+    if(this.modifivalue){
+      this.modifivalue.etats=10;
+      this.panier.updateFacture(this.modifivalue).subscribe(response=>console.log(response));
+      this.displaypaiement = false;
+    }
+  }
   open(modifivalue:detserviceClient,index:number) {
     this.modifivalue=modifivalue;
     this.index=index;
     this.display = true;
   }
-  
-  ngOnDestroy() {
-    this.subscriptions.forEach(sub => sub.unsubscribe());
+  openInvoice(modifivalue:detserviceClient) {
+    console.log("valierere");
+    this.modifivalue=modifivalue;
+    this.displaypaiement = true;
   }
   onPageChange(event: PaginatorState) {
       this.first = event.first ?? 0;
@@ -88,4 +94,22 @@ export class HistoriqueComponent {
         this.servicelist =[]});
     }
   }
+  getTotalInvoice():number{
+    if(this.modifivalue!=null){
+      return this.modifivalue.serviceList.reduce((acc, row) => acc + (row.service?.price ?? 0), 0);
+    }
+    return 0;
+  }
+  getColorInvoinceButon(etat:number){
+    if(etat==10){
+      return "secondary";
+    }
+    return "success";
+  }
+  getColorInvoinceFunct(modifivalue:detserviceClient){
+    if(modifivalue.etats!=10){
+       this.openInvoice(modifivalue);
+    }
+  }
+  
 }
