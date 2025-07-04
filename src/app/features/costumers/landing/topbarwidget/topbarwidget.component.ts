a4c17019a6728a42b8dc2828f48a5248b1d49e72
AlinaMertina <mertinaclaudietoto@gmail.com>
2025-07-04 19:45:56 +0300

    feature(costumer):new home page

--- src/app/features/costumers/landing/topbarwidget/topbarwidget.component.ts
+++ src/app/features/costumers/landing/topbarwidget/topbarwidget.component.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
+import { Component ,AfterViewInit} from '@angular/core';
 import { StyleClassModule } from 'primeng/styleclass';
 import { Router, RouterModule } from '@angular/router';
 import { RippleModule } from 'primeng/ripple';
 import { ButtonModule } from 'primeng/button';
+
 @Component({
   selector: 'app-topbarwidget',
   imports: [StyleClassModule,RouterModule,RippleModule,ButtonModule],
   templateUrl: './topbarwidget.component.html',
   styleUrl: './topbarwidget.component.scss'
 })
-export class TopbarwidgetComponent {
-  constructor(public router: Router) {}
+export class TopbarwidgetComponent implements AfterViewInit{
+  constructor(public router: Router) { }
+  ngAfterViewInit() {
+    const navMenu = document.getElementById("nav-menu");
+    const navLink = document.querySelectorAll(".nav-link");
+    const hamburger = document.getElementById("hamburger");
+
+    if (hamburger && navMenu) {
+      hamburger.addEventListener("click", () => {
+      navMenu.classList.toggle("left-[0]")
+      hamburger.classList.toggle("ri-close-large-line")
+      })
+    }
+    if (navMenu && hamburger) {
+       navLink.forEach(link => {
+          link.addEventListener("click", () => {
+          navMenu.classList.toggle("left-[0]")
+          hamburger.classList.toggle("ri-close-large-line")       
+          })
+      })
+    }
+   
+
+   
+  }
 }
